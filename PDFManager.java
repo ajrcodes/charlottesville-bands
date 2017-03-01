92b3b953a205b2b77a0de20c417e56fa0703f7e9
Ryan-Donovan <rad9vy@virginia.edu>
2017-03-01 18:45:14 -0500

    PDF now reads line by line

--- PDFManager.java
+++ PDFManager.java
@@ -1,15 +1,23 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessFile;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+/* Using apache.pdfbox  */
+/* See radixcode for base code to extract text from pdf */
+/*https://radixcode.com/pdfbox-example-code-how-to-extract-text-from-pdf-file-with-java/*/
 
 public class PDFManager {
     
    private PDFParser parser;
-   private PDFTextStripper pdfStripper;
+   private PDFTextStripper pdfTStripper;
+   private PDFTextStripperByArea pdfStripper;
    private PDDocument pdDoc ;
    private COSDocument cosDoc ;
    
@@ -31,20 +39,43 @@ public class PDFManager {
        
        parser.parse();
        cosDoc = parser.getDocument();
-       pdfStripper = new PDFTextStripper();
-       pdDoc = new PDDocument(cosDoc);
+       pdfStripper = new PDFTextStripperByArea();
+       pdfStripper.setSortByPosition(true);
+       pdfTStripper = new PDFTextStripper();
+       /*pdDoc = new PDDocument(cosDoc);
        pdDoc.getNumberOfPages();
        pdfStripper.setStartPage(1);
-       pdfStripper.setEndPage(pdDoc.getNumberOfPages());
-       
-       Text = pdfStripper.getText(pdDoc);
+       pdfStripper.setEndPage(pdDoc.getNumberOfPages()); */      
+       Text = pdfTStripper.getText(pdDoc);
        pdDoc.close();
+       
+	   /*try{
+		   PDDocument document = null; 
+		   document = PDDocument.load(file);
+		   document.getClass();
+		   if( !document.isEncrypted() ){
+		       PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+		       stripper.setSortByPosition( true );
+		       PDFTextStripper Tstripper = new PDFTextStripper();
+		       String st = Tstripper.getText(document);
+		       System.out.println("Text:"+st);
+		   }
+		   }catch(Exception e){
+		       e.printStackTrace();
+		   }*/
        return Text;
+       
+      
+  
+       
        
    }
 
     public void setFilePath(String filePath) {
         this.filePath = filePath;
+    }
+    public File getFile() {
+    	return this.file;
     }
    
 }
