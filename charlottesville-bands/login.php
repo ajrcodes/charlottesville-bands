d61dbbe4cdbd4ebab53f9ac7727f98c51e9f4b8b
ANDREW ROBERTS <ajrcodes@gmail.com>
2017-04-03 23:58:57 -0400

    Syncing

---
+++ charlottesville-bands/login.php
@@ -0,0 +1,49 @@
+<?php
+
+    // mailer functionality
+    require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+    $mail = new PHPMailer;
+
+	// setup db connection
+	include_once 'db.php';
+
+	// get posted variables
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zipcode = $_POST['zipcode'];
+
+    // insert and query db
+	$insert_q = "INSERT INTO siteusers VALUES('$name', '$email', '$address', '$city', '$state', '$zipcode')";
+	$result = mysqli_query($conn, $insert_q);
+
+    // Send the user a confirmation email
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+    $mail->Mailer   = 'gmail';
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'ajrjazz@gmail.com';                // SMTP username
+    $mail->Password = 'cokevanillaR0x';                   // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    $mail->setFrom('ajrjazz@gmail.com', 'Charlottesville Bands');
+    $mail->addAddress($email);                            // Name is optional
+    $mail->addReplyTo('ajrjazz@gmail.com', 'Information');
+
+    $mail->isHTML(true);                                  // Set email format to HTML
+
+    $mail->Subject = 'Thanks for registering!';
+    $mail->Body    = 'You registered for Charlottesville Bands! <b>Awesome</b>';
+    $mail->AltBody = 'You registered for Charlottesville Bands! Awesome.';
+
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } 
+
+    // will set result to the status of the insertion -- false if the email was already present
+    echo json_encode( array('result' => $result) );
+?>
---
+++ charlottesville-bands/memberpage.php
@@ -0,0 +1,150 @@
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+
+<!--
+	CS4753 - E-commerce
+	Semester Project
+	Group members:  Andrew Roberts (ajr5re), Ryan Donovan (rad9vy)
+
+	Current Version:  Milestone 1
+-->
+<html>
+	<head>
+		<title>Charlottesville Bands</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="index">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header" class="alt">
+					<h1 id="logo"><a href="index.html"><span></span></a></h1>
+					<nav id="nav">
+						<ul>
+							<li class="gray current"><a href="index.html">Home</a></li>
+							<li class="gray"><a href="about.html">About Us</a></li>
+							<li class="gray"><a href="bands.html">The Bands</a></li>
+							<li class="gray"><a href="" class="button special">Login</a></li>
+							<li class="gray"><a href="signup.html" class="button special">Sign Up</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+
+					<!--
+						".inner" is set up as an inline-block so it automatically expands
+						in both directions to fit whatever's inside it. This means it won't
+						automatically wrap lines, so be sure to use line breaks where
+						appropriate (<br />).
+					-->
+					<div class="inner">
+
+						<header>
+							<h2> Welcome, ENTER NAME HERE! </h2>
+						</header>
+						<h2> Charlottesville bands</h2>
+						<footer>
+							<ul class="buttons vertical">
+								<li><a href="#main" class="button fit scrolly">BOOK NOW</a></li>
+							</ul>
+						</footer>
+
+					</div>
+
+				</section>
+
+			<!-- Main -->
+				<article id="main">
+
+					<!-- Three -->
+						<section class="wrapper style3 container special">
+
+							<header class="major">
+								<h2>Select a genre</h2>
+							</header>
+
+							<div class="row">
+								<div class="6u 12u(narrower)">
+
+									<section>
+										<a href="#" class="image featured"><img src="images/jazz_img.png" width = "250" height = "300" alt="" /></a>
+										<header>
+											<a href="#"><h3>Jazz</h3></a>
+										</header>
+									</section>
+
+								</div>
+								<div class="6u 12u(narrower)">
+
+									<section>
+										<a href="#" class="image featured"><img src="images/rock_img.jpg" width = "250" height = "300" alt="" /></a>
+										<header>
+											<a href="#"><h3>ROCK</h3></a>
+										</header>
+									</section>
+
+								</div>
+							</div>
+							<div class="row">
+								<div class="6u 12u(narrower)">
+
+									<section>
+										<a href="#" class="image featured"><img src="images/soul_img.jpg" width = "250" height = "300" alt="" /></a>
+										<header>
+											<a href="#"><h3>SOUL</h3></a>
+										</header>
+									</section>
+
+								</div>
+								<div class="6u 12u(narrower)">
+
+									<section>
+										<a href="#" class="image featured"><img src="images/jambands_img.jpg" width = "250" height = "300" alt="" /></a>
+										<header>
+											<a href="#"><h3>JAM BANDS</h3></a>
+										</header>
+									</section>
+
+								</div>
+							</div>
+
+						</section>
+
+				</article>
+
+			<!-- Footer -->
+				<footer id="footer">
+
+					<ul class="icons">
+					</ul>
+
+					<ul class="copyright">
+						<li>&copy; Charlottesville Bands</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
